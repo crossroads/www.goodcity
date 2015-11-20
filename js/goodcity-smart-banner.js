c162cfbd034267d18fb7513c12f8ea13848b6ce6
Shivani <shivanibhanwal@gmail.com>
2015-11-20 21:50:05 +0530

    Added smart banner to the goodcity index page also added google play and apple badges on index page

---
+++ js/goodcity-smart-banner.js
@@ -0,0 +1,13 @@
+var android = location.href.match(/#android$/) || navigator.userAgent.match(/Android/i) != null;
+var web_ios_win = navigator.userAgent.match(/Macintosh|Windows|Safari/i) != null;
+$.smartbanner({
+  title: 'GoodCity',
+  author: 'Crossroads',
+  icon: '/assets/images/Global-Distribution_logo.jpg',
+  iconGloss: false,
+  hideOnInstall: true,
+  force: web_ios_win ? null : (android ? 'android' :  'ios'),
+  daysHidden: 0,
+  daysReminder: 7,
+  layer: true
+});
